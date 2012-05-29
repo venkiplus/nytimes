33991ff7c4d8a8735eb63c94f403fbb9865e1dfe
Tarun Desikan <tarun@moovweb.com>
2012-05-28 22:03:06 -0700

    re-order functions slightly

--- scripts/html.ts
+++ scripts/html.ts
@@ -4,19 +4,20 @@ $("/html") {
   # We have replaced all the usual importing stylesheets etc. that we apply
   # to every page with some pre-defined functions, found in the functions
   # folder of the project
-  
+
+  absolutize()  # don't proxy img and other assets
+
   removeAllStyles()
-  
+    
   removeScripts()
 
   metaTags()
-
+  
   addAssets()
   
   rewrite()
   
-  absolutize()
-
   @import mappings.ts
-
+  
 }
+
