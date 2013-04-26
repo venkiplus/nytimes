d960f422d572c2d558976f1d022d83d7313ce4d3
Michael JL Catlin <malrase@gmail.com>
2013-04-26 15:44:42 -0700

    Wrapping the day in a span for easier manipulation in a demo

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -3,6 +3,9 @@ $("./body") {
    insert_top("a", href: "/") {
      move_here("//img[@id='mastheadLogo']")
    }
-   move_here("//div[@id='date']")
+   move_here("//div[@id='date']") 
+   $$("#date > p") {
+    wrap_text_children("span", id: "day")
+   } 
  }
 }
