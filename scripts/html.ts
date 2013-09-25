bcca9b0b14b7d0cfe6b2239cacb8b91f3e769728
Tevfik Sertel <tsertel@gmail.com>
2013-09-24 19:07:02 -0500

    removal of scripts that aren't required

--- scripts/html.ts
+++ scripts/html.ts
@@ -8,16 +8,14 @@ $("/html") {
   clean_mobile_meta_tags()
 
   remove_all_styles()
-
+  # Remove all scripts that we don't use
+  remove("//script[@src]")
+  
   add_assets()
 
   @import sections/header.ts
   @import sections/footer.ts
-
+  
   @import mappings.ts
-
-  # remove a frame-busting JS file
-  remove("//script[contains(@src, 'sitewide.js')]") 
-
 }
 
