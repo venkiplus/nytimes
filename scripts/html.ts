eac7f8c76919d05852e3674dc3ffd1cdb1318020
Stephen Moock <stephen.moock@gmail.com>
2014-01-21 05:59:58 -0800

    reverting

--- scripts/html.ts
+++ scripts/html.ts
@@ -7,6 +7,7 @@ $("/html") {
 
   # clean_mobile_meta_tags()
 
+<<<<<<< HEAD
   # remove_all_styles()
   # # Remove all scripts that we don't use
   # remove("//script[@src]")
@@ -21,5 +22,17 @@ $("/html") {
   # # Page Optimizations
   # move_css_above_scripts()
 
+=======
+  remove_all_styles()
+  # Remove all scripts that we don't use
+  remove("//script[@src]")
+  
+  add_assets()
+  
+  @import sections/header.ts
+  @import sections/footer.ts
+  
+  @import mappings.ts
+>>>>>>> parent of 58050a6... doing a demo
 }
 
--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,41 +1,76 @@
-# $("./body") {
-#   # Add a class to the body for page-specific styling
-#   add_class("mw-home")
+$("./body") {
+  # Add a class to the body for page-specific styling
+  
+  add_class("mw-home")
+  
+  $("./div[@id = 'mw-body-content']") {
 
-#   # Main page
-#   $("./div[@id = 'mw-body-content']") {
-#     # Move items we want to keep on the page to the body, out of the .mw-hide divs
-#     scaffold_items()
+ 
+    # Move items we want to keep on the page to the body, out of the .mw-hide divs
+    $(".//div[@id='main']") {
+      $("./div[contains(@class, 'baseLayoutBelowFold')]") {
+        $(".//div[contains(@class, 'abColumn')]") {
+          add_class("mw-keep")
+          move_to("/html/body/div[@id = 'mw-body-content']/header[contains(@class, 'mw-header')]", "after")
+        }
+      }
+    }
+ 
+    $("./div[contains(@class, 'abColumn')]") {
+      $(".//div[contains(@class, 'column')]") {
+        # The following snippet takes the title (which used to be a link) and
+        # turns it into an item in the accordion
+        $("./h6") {
+          $("./a") {
+            # we remove the '»' from the end of the h6
+            remove_string("»")
+            copy_to("ancestor::div[1]/ul", "bottom") {
+              text() {
+                prepend("More ") 
+              }
+              wrap("li")
+              wrap("h6")
+            }
+            name("div")
+          }
+          add_class("mw_bar2")
+          name("div")
+        }
+        $("./ul/li") {
+          $("./h6/a") {
+            add_class("mw_bar1") 
+          }
+          $("./a[contains(@class, 'thumb')]") {
+            add_class("padded pull-left")
+          }
+        }
+      
+      
+        # # make a uranium toggler where first element is the button and second is the content 
+        ur_toggler("./div","./ul")
+      }
+      
+      
+      # Implementing the image carousel at the top of the page
+      insert_before("div", class: "mw-keep") {      
+        ur_carousel("/html/body/div[@id = 'mw-body-content']/div[@id='shell']//table[@id='insideNYTimesBrowser']//a[img]")
+        $("./div[@data-ur-carousel-button-type]") {
+          inner("")
+        }
+        $(".//div[@data-ur-carousel-component = 'count']") {
+          add_class("mw-hide")
+        }
+        $(".//div[@data-ur-carousel-component = 'view_container']") {
+          insert_bottom("div", data-ur-carousel-component: "dots", class: "mw-dots")
+        }
+        # enable auto scroll, ability to swipe and fill width with 1 item
+        attributes(data-ur-autoscroll: "enabled", data-ur-touch: "enabled", data-ur-fill: "1")
+       }
+       
+      }
+  
+    $("./*[not(contains(@class, 'mw-keep'))]") {
+     add_class("mw-hide")
+    }
+  }
 
-#     $("./div[contains(@class, 'abColumn')]") {
-#       $(".//div[contains(@class, 'column')]") {
-#         # The following snippet takes the title (which used to be a link) and
-#         # turns it into an item in the accordion
-#         # accordionize_items()
-
-#         # make a uranium toggler where first element is the button and second is the content
-#         # ur_toggler("./div","./ul")
-#       }
-#       # Implementing the image carousel at the top of the page
-#       # insert_before("div", class: "mw-keep") {
-#       #   ur_carousel("/html/body/div[@id = 'mw-body-content']/div[@id='shell']//table[@id='insideNYTimesBrowser']//a[img]")
-#       #   beautify_carousel()
-#       # }
-#     }
-#     # Hide everything that isn't marked to keep
-#     template_clean()
-
-#     # Optimize Images
-#     $(".//a[contains(concat(' ', @class, ' '), ' thumb ')]") {
-#       $("./img") {
-#         # should drop images from ~7k to ~1k
-#         perf.optimize_image() {
-#           # perf.format("webp")
-#           # perf.height("50")
-#           # perf.width("50")
-#           # perf.quality("70")
-#         }
-#       }
-#     }
-#   }
-# }
